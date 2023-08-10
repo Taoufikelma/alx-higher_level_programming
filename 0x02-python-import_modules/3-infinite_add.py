dd6491ad3a1cacec74aed80247b9a7af02fdc4d8
taoufik <taoufik@gmail.com>
2023-08-10 13:44:55 +0000

    python project import and modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-a = len(sys.argv)
-sum = 0
-for i in range(a - 1):
-	sum += int(sys.argv[i+1])
-print("{}".format(sum))
+if __name__ == "__main__":
+	import sys
+	a = len(sys.argv)
+	sum = 0
+	for i in range(a - 1):
+		sum += int(sys.argv[i+1])
+	print("{}".format(sum))
