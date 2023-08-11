6cf06da413d663ff6dad27ecc12d377d77e1ebf4
taoufik <taoufik@gmail.com>
2023-08-11 09:01:33 -0700

    python project import and modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
-argv_len = len(sys.argv) - 1
+argv_len = len(argv[1:])
 if argv_len != 3:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
-elif sys.argv[2] not in ("+", "-", "*", "/"):
+elif argv[2] not in ("+", "-", "*", "/"):
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 else:
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if sys.argv[2] == "+":
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sys.argv[2] == "-":
+    elif argv[2] == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif sys.argv[2] == "*":
+    elif argv[2] == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif sys.argv[2] == "/":
+    elif argv[2] == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
