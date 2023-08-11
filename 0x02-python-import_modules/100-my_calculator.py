832197a81741b4ebd4a988047cc64284356b36aa
taoufik <taoufik@gmail.com>
2023-08-11 07:24:44 -0700

    python project import and modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,10 +13,10 @@ else:
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if sys.argv[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    if sys.argv[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    if sys.argv[2] == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    if sys.argv[2] == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
