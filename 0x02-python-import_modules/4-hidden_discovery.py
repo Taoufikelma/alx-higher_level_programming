d620e37007bae31ac4db39c70292e124daa0dd7f
taoufik <taoufik@gmail.com>
2023-08-11 06:39:39 -0700

    python project import and modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    sum_name = dir(hidden_4)
-    for name in sum_name:
+    file_name = dir(hidden_4)
+    for name in file_name:
         if name[:2] != '__':
-        print("{}".format(name))
+            print(name)
