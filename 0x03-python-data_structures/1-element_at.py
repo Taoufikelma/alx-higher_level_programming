b1ca0d15f90d786413473cac30d0cfff4e1f9392
taoufik <taoufik@gmail.com>
2023-08-12 06:13:27 -0700

    python data structure

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    list_len = int(len(my_list))
-    idx = int(idx)
-    if idx > 0 or idx < list_len:
-        print("Element at index {} is {}".format(idx, my_list[idx]))
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
 
 
-return None
+return("Element at index {} is {}".format(idx, my_list[idx]))
