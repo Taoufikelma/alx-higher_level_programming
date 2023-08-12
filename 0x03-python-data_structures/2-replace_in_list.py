de9faf8862914cc243747c4c0baebe1559b72d69
taoufik <taoufik@gmail.com>
2023-08-12 09:10:09 -0700

    python data structure

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,4 +6,4 @@ def replace_in_list(my_list, idx, element):
         for i in range(len(my_list)):
             if idx == i:
                 my_list[idx] = element
-                return my_list
+    return my_list
