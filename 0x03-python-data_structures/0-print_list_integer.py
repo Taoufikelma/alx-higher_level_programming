83e4d6f211354e13cef8fdf073fc1e9ee2e6c4e9
taoufik <taoufik@gmail.com>
2023-08-12 05:28:16 -0700

    python data structure

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     len_list = len(my_list)
-    for i in len_list:
-        print("{}".format(i))
+    for i in range(len_list):
+        print("{}".format(my_list[i]))
