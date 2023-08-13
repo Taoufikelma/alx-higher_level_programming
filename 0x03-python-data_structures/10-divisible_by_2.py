ea8ef6b065752b0b6589565e031cc56d42d91a99
taoufik <taoufik@gmail.com>
2023-08-13 12:22:28 -0700

    python data structure

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     copy_list = my_list[:]
-    for i in range(len(my_list)):
+    for i in range(len(copy_list)):
         if i % 2 == 0:
             copy_list[i] = True
         else:
