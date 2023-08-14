79d0fdd27754744b291a30789b3c46fd4b78e9f9
taoufik <taoufik@gmail.com>
2023-08-14 07:14:09 -0700

    python data structure

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,6 @@
 def multiple_returns(sentence):
     len_of_sentence = len(sentence[:])
     first_char = sentence[0]
-    if len_of_sentence > 0:
-        resultat = len_of_sentence, first_char
-    else:
+    if len_of_sentence < 0:
         return None
-    return resultat
+    return len_of_sentence, first_char
