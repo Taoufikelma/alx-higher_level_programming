6129bfa1c0747f81b9a7b2278940dbc5f52535f4
taoufik <taoufik@gmail.com>
2024-01-10 11:58:35 -0800

    js we jquery

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,25 +2,23 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const args = process.argv;
+const movieId = args[2];
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
-    }
-  })
-  .then(movie => {
-    console.log(`Characters in ${movie.title}:`);
-    return Promise.all(movie.characters.map(characterUrl => request(characterUrl)));
-  })
-  .then(responses => Promise.all(responses.map(response => response.json())))
-  .then(characters => {
-    characters.forEach(character => console.log(character.name));
-  })
-  .catch(error => {
-    console.error(error.message);
+sendRequest(movieUrl, ({ characters }) => {
+  characters.forEach(url => {
+    sendRequest(url, ({ name }) => console.log(name));
   });
+});
+
+function sendRequest (url, fn) {
+  request(url, (err, resp, body) => {
+    if (err) return console.log(err);
+    if (resp.statusCode !== 200) console.log('Request failed');
+
+    const bodyJson = JSON.parse(body);
+    if (fn) fn(bodyJson);
+    return bodyJson;
+  });
+}
